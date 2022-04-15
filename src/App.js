a0f8084571a8f1d61d7557c56c225f15fbc1356c
Marcos Rampaso <49240709+Mazner@users.noreply.github.com>
2022-04-15 15:36:18 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import Footer from './components/layout/footer/Footer';
+//import Header from './components/layout/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
